32a669f6e7ec595a3fb5a700a6731a9cbc90e2d9
Stefano Fusai <stefanofusai@gmail.com>
2026-07-07 12:24:22 +0200

    feat: make user email unique

--- {{cookiecutter.project_slug}}/src/apps/core/models.py
+++ {{cookiecutter.project_slug}}/src/apps/core/models.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class CreatedAtModel(models.Model):
@@ -19,6 +20,8 @@ class CreatedAtUpdatedAtModel(CreatedAtModel):
 
 
 class User(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
+
     class Meta:
         ordering = ("username",)
         verbose_name = AbstractUser.Meta.verbose_name
--- {{cookiecutter.project_slug}}/tests/factories.py
+++ {{cookiecutter.project_slug}}/tests/factories.py
@@ -8,7 +8,7 @@ from apps.notes.models import Note
 
 class UserFactory(factory.django.DjangoModelFactory):
     username = factory.Sequence(lambda n: f"user-{n}")
-    email = factory.Faker("email")
+    email = factory.Sequence(lambda n: f"user-{n}@example.com")
 
     class Meta:
         model = User
