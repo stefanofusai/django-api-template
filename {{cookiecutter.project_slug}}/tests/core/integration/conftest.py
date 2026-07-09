ebc7453b4453ebf75bed9a811df53c5dfc5d1864
Stefano Fusai <stefanofusai@gmail.com>
2026-07-09 16:43:11 +0200

    fix: repair generated CI checks

--- {{cookiecutter.project_slug}}/tests/core/integration/conftest.py
+++ {{cookiecutter.project_slug}}/tests/core/integration/conftest.py
@@ -1,8 +1,8 @@
-{% if cookiecutter.use_example_api == "yes" and cookiecutter.api_auth == "token" -%}
+{%- if cookiecutter.use_example_api == "yes" and cookiecutter.api_auth == "token" -%}
 from pytest_factoryboy import LazyFixture, register
 
 from tests.factories import TokenFactory, UserFactory
 
 register(UserFactory, "user_1")
 register(TokenFactory, "token_1", user=LazyFixture("user_1"))
-{%- endif %}
+{% endif -%}
